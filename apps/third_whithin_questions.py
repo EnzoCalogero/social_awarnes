3f7735b59deb5a868255060a8b6c2e5e79f7f2b6
Enzo Calogero <EnzoCalogero@users.noreply.github.com>
2023-11-11 18:53:53 +0000

    multi pages design 2023 11 11

--- apps/third_whithin_questions.py
+++ apps/third_whithin_questions.py
@@ -16,29 +16,13 @@ config = {
 dataset = pd.read_csv("C:/gits_folders/social_awarnes/data/violence_data.csv")
 
 Education = dataset[dataset["Demographics Question"] == "Education"]
-Education = Education.groupby(['Demographics Response', 'Gender'])["Value"]\
-                                            .agg(["median", "max", "min", "mean"]).reset_index()
-Education = Education.sort_values(by=['median'], ascending=False)
-
 Marital_status = dataset[dataset["Demographics Question"] == "Marital status"]
-Marital_status = Marital_status.groupby(['Demographics Response', 'Gender'])["Value"]\
-                                            .agg(["median", "max", "min", "mean"]).reset_index()
-Marital_status = Marital_status.sort_values(by=['median'], ascending=False)
-
 Age = dataset[dataset["Demographics Question"] == "Age"]
-Age = Age.groupby(['Demographics Response', 'Gender'])["Value"]\
-                                                .agg(["median", "max", "min", "mean"]).reset_index()
-Age = Age.sort_values(by=['median'], ascending=False)
-
 Residence = dataset[dataset["Demographics Question"] == "Residence"]
-Residence = Residence.groupby(['Demographics Response', 'Gender'])["Value"]\
-                                                .agg(["median", "max", "min", "mean"]).reset_index()
-Residence = Residence.sort_values(by=['median'], ascending=False)
 
 country = dataset.sort_values(by=['Country'], ascending=True)
 country = country['Country'].unique()
 country = np.concatenate((country, ["All Countries"]))
-
 
 layout = html.Div([
     dbc.Row(
@@ -58,22 +42,22 @@ layout = html.Div([
                 value='All Countries',
                 multi=True,
                 clearable=False, ),
-            width={"size": 4, },)
+            width={"size": 4, }, )
     ]),
 
     dbc.Row([
-            dbc.Col(dcc.Graph(id="education_graph",
-                              config=config), width=6),
-            dbc.Col(dcc.Graph(id="Residence_graph",
-                              config=config), width=6),
-            ]),
+        dbc.Col(dcc.Graph(id="education_graph",
+                          config=config), width=6),
+        dbc.Col(dcc.Graph(id="Residence_graph",
+                          config=config), width=6),
+    ]),
 
     dbc.Row([
-             dbc.Col(dcc.Graph(id="Age_graph",
-                               config=config), width=6),
-             dbc.Col(dcc.Graph(id="Marital status_graph",
-                               config=config), width=6),
-             ]),
+        dbc.Col(dcc.Graph(id="Age_graph",
+                          config=config), width=6),
+        dbc.Col(dcc.Graph(id="Marital status_graph",
+                          config=config), width=6),
+    ]),
     dbc.Row(
         dbc.Col([
             html.P(dcc.Link('Next Page', href='/Full_Details')),
@@ -82,23 +66,31 @@ layout = html.Div([
             width={"size": 6, "offset": 3},
         )),
 
-
-         ], style={'padding': '0px 10px 15px 10px',
-                   'marginL,eft': 'auto',
-                   'marginRight': 'auto',
-                   'width': "140vh",
-                   'display': 'inline-block',
-                   'boxShadow': '0px 0px 5px 5px rgba(37,52,113,0.4)'
-                   }
-         )
+], style={'padding': '0px 10px 15px 10px',
+          'marginL,eft': 'auto',
+          'marginRight': 'auto',
+          'width': "140vh",
+          'display': 'inline-block',
+          'boxShadow': '0px 0px 5px 5px rgba(37,52,113,0.4)'
+          }
+)
 
 
 @app.callback(
     Output("education_graph", "figure"),
     Input("country2_id", "value"),
 )
-def update_education(country_):
-    fig = px.bar(Education, x='Demographics Response',
+def update_education(country_dict):
+
+    if "All Countries" in country_dict:
+        df_1_ = Education
+    else:
+        df_1_ = Education[Education.Country.isin(country_dict)]
+
+    df_1_ = df_1_.groupby(['Demographics Response', 'Gender'])["Value"] \
+            .agg(["median", "max", "min", "mean"]).reset_index()
+    df_1_ = df_1_.sort_values(by=['median'], ascending=False)
+    fig = px.bar(df_1_, x='Demographics Response',
                  y='median',
                  color='Gender',
                  color_discrete_sequence=["pink", "blue"])
@@ -114,7 +106,7 @@ def update_education(country_):
             title='% Expected Violence (global Median)',
             titlefont_size=16,
             tickfont_size=14,
-            color='crimson',),
+            color='crimson', ),
 
         xaxis=dict(
             title='',
@@ -133,8 +125,17 @@ def update_education(country_):
     Output("Marital status_graph", "figure"),
     Input("country2_id", "value"),
 )
-def update_marital(country_):
-    fig = px.bar(Marital_status, x='Demographics Response',
+def update_marital(country_dict):
+    if "All Countries" in country_dict:
+        df_1_ = Marital_status
+    else:
+        df_1_ = Marital_status[Marital_status.Country.isin(country_dict)]
+
+    df_1_ = df_1_.groupby(['Demographics Response', 'Gender'])["Value"] \
+        .agg(["median", "max", "min", "mean"]).reset_index()
+    df_1_ = df_1_.sort_values(by=['median'], ascending=False)
+
+    fig = px.bar(df_1_, x='Demographics Response',
                  y='median',
                  color='Gender',
                  color_discrete_sequence=["pink", "blue"])
@@ -150,7 +151,7 @@ def update_marital(country_):
             title='',
             titlefont_size=16,
             tickfont_size=14,
-            color='crimson',),
+            color='crimson', ),
 
         xaxis=dict(
             title='',
@@ -169,8 +170,17 @@ def update_marital(country_):
     Output("Residence_graph", "figure"),
     Input("country2_id", "value"),
 )
-def update_residence(country_):
-    fig = px.bar(Residence, x='Demographics Response',
+def update_residence(country_dict):
+
+    if "All Countries" in country_dict:
+        df_1_ = Residence
+    else:
+        df_1_ = Residence[Residence.Country.isin(country_dict)]
+
+    df_1_ = df_1_.groupby(['Demographics Response', 'Gender'])["Value"] \
+        .agg(["median", "max", "min", "mean"]).reset_index()
+    df_1_ = df_1_.sort_values(by=['median'], ascending=False)
+    fig = px.bar(df_1_, x='Demographics Response',
                  y='median',
                  color='Gender',
                  color_discrete_sequence=["pink", "blue"])
@@ -185,7 +195,7 @@ def update_residence(country_):
             title='',
             titlefont_size=16,
             tickfont_size=14,
-            color='crimson',),
+            color='crimson', ),
         legend_title_text="gender of the surveyed",
         showlegend=True,
         xaxis=dict(
@@ -205,8 +215,18 @@ def update_residence(country_):
     Output("Age_graph", "figure"),
     Input("country2_id", "value"),
 )
-def update_age(country_):
-    fig = px.bar(Age, x='Demographics Response',
+def update_age(country_dict):
+
+    if "All Countries" in country_dict:
+        df_1_ = Age
+    else:
+        df_1_ = Age[Age.Country.isin(country_dict)]
+
+    df_1_ = df_1_.groupby(['Demographics Response', 'Gender'])["Value"] \
+        .agg(["median", "max", "min", "mean"]).reset_index()
+    df_1_ = df_1_.sort_values(by=['median'], ascending=False)
+
+    fig = px.bar(df_1_, x='Demographics Response',
                  y='median',
                  color='Gender',
                  color_discrete_sequence=["pink", "blue"])
@@ -219,11 +239,11 @@ def update_age(country_):
             'yanchor': 'top'},
         yaxis=dict(
             title='',
-            ),
+        ),
         showlegend=False,
         xaxis=dict(
             title='',
-         ),
+        ),
     )
     fig.update_xaxes(tickangle=45,
                      tickfont=dict(
